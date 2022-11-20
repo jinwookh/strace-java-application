e89d98ee5077755b44c28c9d533fb028f55603a0
jinwook han <jin942002@naver.com>
2022-11-20 13:46:00 +0900

    add putChar
    add code to make byteBuffer to be used.
    byteBuffer code seems to be deleted by compiler if it is not used.

--- src/main/java/strace/java/application/practice/NIODirectAllocationTest.java
+++ src/main/java/strace/java/application/practice/NIODirectAllocationTest.java
@@ -10,10 +10,12 @@ public class NIODirectAllocationTest {
     public static void main(String[] args) {
         System.out.println("byteBuffer allocation start..");
         ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+        byteBuffer.putChar('c');
         System.out.println("byteBuffer allocated!");
 
         System.out.println("byteBuffer direct allocation start..");
         ByteBuffer directByteBuffer = ByteBuffer.allocateDirect(10);
+        directByteBuffer.putChar('c');
         System.out.println("byteBuffer directly allocated!");
     }
 }
